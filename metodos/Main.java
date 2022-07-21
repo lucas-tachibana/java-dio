281a5fe80e68ded2def147a64241424096d2e5b7
lucas-tachibana <lucas_tachibana@hotmail.com>
2022-07-21 17:35:37 -0300

    Initial Commit

---
+++ metodos/Main.java
@@ -0,0 +1,16 @@
+package metodos;
+
+public class Main {
+    public static void main(String[] args) {
+        // Calculadora
+        System.out.println("Exercício calculadora:");
+        Calculadora.soma(3, 4);
+
+        Calculadora.subtracao(7, 3);
+
+        Calculadora.multiplicacao(7, 2);
+
+        Calculadora.divisao(16, 4);
+
+    }
+}
