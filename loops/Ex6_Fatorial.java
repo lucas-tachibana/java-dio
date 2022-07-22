995f8889ee727d0fcd63730f96de8d98c4227d51
lucas-tachibana <lucas_tachibana@hotmail.com>
2022-07-22 11:19:10 -0300

    Adiciona exercício 06, cálculo de fatorial

---
+++ loops/Ex6_Fatorial.java
@@ -0,0 +1,21 @@
+package loops;
+
+import java.util.Scanner;
+
+public class Ex6_Fatorial {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite o número para calcular fatorial");
+        int fatorial = scan.nextInt();
+        int total = 1;
+        for (int i = fatorial; i > 0; i--) {
+            total *= i;
+            System.out.println("total: " + total);
+
+        }
+
+        System.out.println("O resultado de " + fatorial + "! é " + total);
+
+        scan.close();
+    }
+}
