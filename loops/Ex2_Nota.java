ff43b83d04bd21c26087f882262a7e915b0e660b
lucas-tachibana <lucas_tachibana@hotmail.com>
2022-07-22 10:23:41 -0300

    Adiciona exercício 02 de loops, nota de alunos

---
+++ loops/Ex2_Nota.java
@@ -0,0 +1,20 @@
+package loops;
+
+import java.util.Scanner;
+
+public class Ex2_Nota {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int nota;
+
+        System.out.println("Digite o valor da nota:");
+        nota = scan.nextInt();
+        while (nota < 0 || nota > 10) {
+            System.out.println("Nota inválida!!! Digite novamente: ");
+            nota = scan.nextInt();
+        }
+        scan.close();
+
+    }
+}
