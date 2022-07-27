201123bd4696407acda5d6de6afed85a665aa6f9
lucas-tachibana <lucas_tachibana@hotmail.com>
2022-07-27 11:06:57 -0300

    "Adiciona Classe Gato"

--- primeiro-programa/src/br/com/dio/PrimeiroPrograma.java
+++ primeiro-programa/src/br/com/dio/PrimeiroPrograma.java
@@ -1,7 +1,14 @@
 package br.com.dio;
 
+import br.com.dio.model.Gato;
+
 public class PrimeiroPrograma {
     public static void main(String[] args) {
+        Gato gato1 = new Gato();
+        gato1.setNome("Mimi");
+        gato1.setIdade(7);
+        gato1.setCor("Branco");
 
+        System.out.println(gato1.toString());
     }
 }
